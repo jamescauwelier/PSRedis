d95c3a2711cac50914d78fefeb1f6c6b243af63f
James Cauwelier <james@sparkcentral.com>
2014-06-23 23:47:34 +0200

    Add sentinel nodes to a monitor set

--- tests/RedisSentinel/MonitorSetTest.php
+++ tests/RedisSentinel/MonitorSetTest.php
@@ -7,11 +7,34 @@ class MonitorSetTest extends \PHPUnit_Framework_TestCase
 {
     private $monitorSetName = 'name-of-monitor-set';
 
+    /**
+     * @return \RedisSentinel\SentinelNode
+     */
+    private function createMockedSentinelNode()
+    {
+        $sentinelNode = \Phake::mock('RedisSentinel\SentinelNode');
+
+        return $sentinelNode;
+    }
+
     public function testAMonitorSetHasAName()
     {
         $monitorSet = new MonitorSet($this->monitorSetName);
         $this->assertEquals($this->monitorSetName, $monitorSet->getName(), 'A monitor set is identified by a name');
     }
 
+    public function testThatSentinelNodesCanBeAddedToMonitorSets()
+    {
+        $monitorSet = new MonitorSet($this->monitorSetName);
+        $monitorSet->addNode($this->createMockedSentinelNode());
+        $this->assertAttributeCount(1, 'nodes', $monitorSet, 'Sentinel node can be added to a monitor set');
+    }
+
+    public function testThatOnlySentinelNodeObjectsCanBeAddedAsNode()
+    {
+        $this->setExpectedException('\\PHPUnit_Framework_Error', 'Argument 1 passed to RedisSentinel\MonitorSet::addNode() must be an instance of RedisSentinel\SentinelNode');
+        $monitorSet = new MonitorSet($this->monitorSetName);
+        $monitorSet->addNode(new \StdClass());
+    }
 }
  
