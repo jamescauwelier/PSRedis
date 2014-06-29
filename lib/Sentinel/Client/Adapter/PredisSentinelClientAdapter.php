7d2d6035990b0e9ac38d93a67ef26f4b2b6e30b7
James Cauwelier <james.cauwelier@gmail.com>
2014-06-28 17:30:49 -0700

    Extend the Predis client with two commands

--- lib/Sentinel/Client/Adapter/PredisSentinelClientAdapter.php
+++ lib/Sentinel/Client/Adapter/PredisSentinelClientAdapter.php
@@ -2,14 +2,32 @@
 
 namespace Sentinel\Client\Adapter;
 
+use Sentinel\Client\Adapter\Predis\Command\GetMasterAddressCommand;
+use Sentinel\Client\Adapter\Predis\Command\RoleCommand;
 use Sentinel\Client\SentinelClientAdapter;
 
 class PredisSentinelClientAdapter
     extends AbstractSentinelClientAdapter
     implements SentinelClientAdapter
 {
+    /**
+     * @var \Predis\Client
+     */
+    private $predisClient;
+
     public function connect()
     {
+        $this->predisClient = new \Predis\Client($this->getPredisClientParameters());
+        $this->predisClient->getProfile()->createCommand(new GetMasterAddressCommand());
+        $this->predisClient->getProfile()->createCommand(new RoleCommand());
+    }
 
+    private function getPredisClientParameters()
+    {
+        return array(
+            'scheme'    => 'tcp',
+            'host'      => $this->ipAddress,
+            'port'      => $this->port,
+        );
     }
 } 
