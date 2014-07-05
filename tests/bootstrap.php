d3f3c463df4c86ca98a281445a0747b3cdef21cc
James Cauwelier <james.cauwelier@gmail.com>
2014-07-05 14:53:58 -0700

    Re-configuration of phpunit to also allow the running of integration tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,6 +6,6 @@ require_once __DIR__.'/../vendor/autoload.php';
 \Phake::setClient(\Phake::CLIENT_PHPUNIT);
 
 // loading mocks
-require_once __DIR__ . '/Redis/Client/Adapter/Predis/Mock/AbstractMockedPredisClientCreator.php';
-require_once __DIR__ . '/Redis/Client/Adapter/Predis/Mock/MockedPredisClientCreatorWithNoMasterAddress.php';
-require_once __DIR__ . '/Redis/Client/Adapter/Predis/Mock/MockedPredisClientCreatorWithMasterAddress.php';
+require_once __DIR__ . '/unit/Redis/Client/Adapter/Predis/Mock/AbstractMockedPredisClientCreator.php';
+require_once __DIR__ . '/unit/Redis/Client/Adapter/Predis/Mock/MockedPredisClientCreatorWithNoMasterAddress.php';
+require_once __DIR__ . '/unit/Redis/Client/Adapter/Predis/Mock/MockedPredisClientCreatorWithMasterAddress.php';
